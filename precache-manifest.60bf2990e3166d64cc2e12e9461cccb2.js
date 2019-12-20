27658a443bbf67be52ce0e0afe92bbc41ef20172
Bhagat <megha.bhagat@accenture.com>
2019-12-21 00:30:51 +0530

    Updates

---
+++ precache-manifest.60bf2990e3166d64cc2e12e9461cccb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29240d23b85d20d863739fccd920898c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf27676eca460f9039d3",
+    "url": "/static/css/main.0f35992d.chunk.css"
+  },
+  {
+    "revision": "5740770b44a7d5f5b72e",
+    "url": "/static/js/2.765abfc1.chunk.js"
+  },
+  {
+    "revision": "022c664b82956dcf39adefd5dcb2f586",
+    "url": "/static/js/2.765abfc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf27676eca460f9039d3",
+    "url": "/static/js/main.a0d5f5d7.chunk.js"
+  },
+  {
+    "revision": "b510c592c102cfe9db60",
+    "url": "/static/js/runtime-main.73f36e82.js"
+  },
+  {
+    "revision": "507c0ddc4350f9bdda96b9846f0c0a78",
+    "url": "/static/media/404-Error.507c0ddc.png"
+  }
+]);
